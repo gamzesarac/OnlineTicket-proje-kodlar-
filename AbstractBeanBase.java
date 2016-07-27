c2123801725ec9a78c994dc05cf26aa8f167f073
ececiva <ece.933@hotmail.com>
2016-07-27 10:37:47 +0300

    Update AbstractBeanBase.java

--- AbstractBeanBase.java
+++ AbstractBeanBase.java
@@ -1,13 +1,17 @@
-package com.mybiletix.bean;
+package bean;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import entity.User;
 
 public abstract class AbstractBeanBase {
 	
+
 	public void addMessage(String summary) {
 		addMessage(summary, null);
 	}
@@ -20,7 +24,8 @@ public abstract class AbstractBeanBase {
 		FacesMessage msg = new FacesMessage(severity, summary, detail);
         FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
-	
+	/**
+	 * Warning messages are shown for the screen*/
 	public void addWarningMessage(String summary) {
 		addMessage(FacesMessage.SEVERITY_WARN, summary, null);
 	}
@@ -35,5 +40,15 @@ public abstract class AbstractBeanBase {
 	
 	public HttpSession getSession(boolean create) {
 		return (HttpSession) getExternalContext().getSession(create);
+	}
+	
+	public String getParam(String paramKey) {
+		HttpServletRequest request = (HttpServletRequest) getExternalContext().getRequest();
+		return request.getParameter(paramKey);
+	}
+	
+	public User getLogedInUser() {
+		UserData userData = (UserData) getSession(false).getAttribute(Constants.USER_DATA);
+		return userData.getUser();//login olmuş olan user
 	}
 }
